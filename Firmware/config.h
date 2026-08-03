bfcb3efa4a78b04e9b3f73abc10755a5439dcfee
abdulrehman8912 <abdul.rehman683738@gmail.com>
2026-08-02 22:07:12 -0400

    Add files via upload
    This the firmware

---
+++ Firmware/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define VENDOR_ID    0xFEED
+#define PRODUCT_ID   0x0000
+#define DEVICE_VER   0x0001
+
+#define MATRIX_ROWS 3
+#define MATRIX_COLS 3
+
+#define DEBOUNCE 5
+
+#define OLED_DISPLAY_128X32 
+#define OLED_TIMEOUT 120000 
+
